77c5ff2c0fdfce07b113cc9e264947fabde3d804
bharat-reddy-male <bharatreddy1994@gmail.com>
2020-10-07 13:57:09 -0500

    sorter for bonus question

---
+++ bonus-sorter.py
@@ -0,0 +1,17 @@
+# Case 1 - Sorter using Files
+# Easy to test
+# Not needed for Hadoop (Hadoop performs this automatically)
+
+# An inplace sort is good for small data only 
+# This step is done automatically in Hadoop
+
+with open("bonus-mapper-output.txt", "r") as unsorted:
+  with open("bonus-sorted.txt", "w") as sorted:
+
+    dataList = unsorted.readlines()
+    dataList.sort(reverse=True)
+
+    # output sorted intermediate key-value pairs
+    for line in dataList:
+      sorted.write(line)  # output
+      print (line)        # display to console
